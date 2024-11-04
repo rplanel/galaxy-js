db4eee60d4624e5536c818793c20b689fa9ce915
Remi  PLANEL <rplanel@pasteur.fr>
2024-11-04 13:59:00 +0100

    try add cookie session

--- src/GalaxyClient.ts
+++ src/GalaxyClient.ts
@@ -23,7 +23,7 @@ export class GalaxyClient {
         'x-api-key': apiKey,
         'accept': 'application/json, text/plain, */*',
         'Content-Type': 'application/json',
-        'Cookie': 'galaxysession=3ce3e2281e4506fb5d1455b3a2f405b9575788704745930e9740ee41808187ceef17fb24a6b062bc',
+        'Cookie': 'galaxysession=3ce3e2281e4506fbe4936bff63a07ce914566a86c755c5791fb3ba914dbf34ee95e8321549bd90a3',
       },
 
       baseURL: this.url,
